b6cbd91fb4b69f4ae4d9d8add43a3c6e68bb54f7
michel_32 <karas.benjamin@gmail.com>
2026-03-19 12:04:49 +0100

    add more tests

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: michel_32 <michel_32@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 15:34:52 by michel_32         #+#    #+#             */
-/*   Updated: 2026/03/19 11:45:39 by michel_32        ###   ########.fr       */
+/*   Updated: 2026/03/19 12:04:03 by michel_32        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,29 @@ int main(void)
 {
     try
     {
-        Bureaucrat Mark = Bureaucrat("Mark Scout", 160);
-        Bureaucrat Amanda = Bureaucrat("Amanda", 0);
-        
+        // Bureaucrat Mark = Bureaucrat("Mark Scout", 160);
+        // Bureaucrat Helly = Bureaucrat("Helly R.", 0);
+        Bureaucrat Irving = Bureaucrat("Irving Bailiff", 32);
+        std::cout << Irving << std::endl;
+        Irving.decrementGrade();
+        Irving.decrementGrade();
+        std::cout << "Irving grade should be 34: " << Irving << std::endl;
+        Irving.incrementGrade();
+        Irving.incrementGrade();
+        Irving.incrementGrade();
+        Irving.incrementGrade();
+        std::cout << "Irving grade should be 30: " << Irving << std::endl;
+
+        //Grade too high exception on incrementation
+        // Bureaucrat Seth = Bureaucrat("Seth Milchick", 1);
+        // std::cout << Seth << std::endl;
+        // Seth.incrementGrade();
+
+        //Grade too low exception on decrementation
+        Bureaucrat PoubelleDuParc = Bureaucrat("La Poubelle du Parc Astérix", 150);
+        std::cout << PoubelleDuParc << std::endl;
+        PoubelleDuParc.decrementGrade();
+
     }
     catch (Bureaucrat::GradeTooLowException &e)
     {
